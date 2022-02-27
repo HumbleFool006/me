ae72b5725ffc438077d1beb6273a925de2de3ef0
humblefool <arunagiriswaran@uber.com>
2022-02-27 23:23:08 +0530

    custom domain integration

---
+++ build/precache-manifest.f65dde4b1c1b49f5c56c704f799f0df5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf7d5904fd38cfe788b4",
+    "url": "/static/css/main.41a66cb4.chunk.css"
+  },
+  {
+    "revision": "cf7d5904fd38cfe788b4",
+    "url": "/static/js/main.cf7d5904.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "e3c3948adcc27092600b5ce829e02f04",
+    "url": "/index.html"
+  }
+];
